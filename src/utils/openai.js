caca0b6a93a82a4917057807a3544ba7a2852709
Your Name <kanukaharish@gmail.com>
2023-09-09 15:25:20 +0530

    Open Ai key updated

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -7,6 +7,3 @@ const openai = new OpenAI({
 });
 
 export default openai;
-
-// // R = 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
-// // REACT_APP_TMDB_KEY = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzI4NjMzZWRiZTEyOWQzODNlZWU5YzY3Y2UyYjFmMyIsInN1YiI6IjY0ZjQwMjM0M2Q0M2UwMDBjNGE4YzJhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jYkMCbIeHJHG6EwXnbiYIUxYlrPkFrAjlZMY8DmwUtQ
